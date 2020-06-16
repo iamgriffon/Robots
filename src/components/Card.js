d8bbb983baa740a96ce1f568623ea46dbac7d70f
Gustavo Dupin <55361396+iamgriffon@users.noreply.github.com>
2020-06-16 18:00:15 -0300

    Made some changes since Git deleted my old repo lol

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
+const Card = ({ name, email, id, phone }) => {
   return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+    <div className='tc grow bg-light-green bw2 ba bw2 br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='robots' className='ba bw1' src={`https://robohash.org/${id}?set=any`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
+        <p>Phone: {phone}</p>
       </div>
     </div>
   );
--- src/robots.js
+++ src/robots.js
@@ -3,60 +3,77 @@ export const robots = [
     id: 1,
     name: 'Leanne Graham',
     username: 'Bret',
-    email: 'Sincere@april.biz'
+    email: 'Sincere@april.biz',
+    rank: '2'
   },
   {
     id: 2,
     name: 'Ervin Howell',
     username: 'Antonette',
-    email: 'Shanna@melissa.tv'
+    email: 'Shanna@melissa.tv',
+    rank: '3'
   },
   {
     id: 3,
     name: 'Clementine Bauch',
     username: 'Samantha',
-    email: 'Nathan@yesenia.net'
+    email: 'Nathan@yesenia.net',
+    rank: 4
+
   },
   {
     id: 4,
     name: 'Patricia Lebsack',
     username: 'Karianne',
-    email: 'Julianne.OConner@kory.org'
+    email: 'Julianne.OConner@kory.org',
+    rank: '3'
+
   },
   {
     id: 5,
     name: 'Chelsey Dietrich',
     username: 'Kamren',
-    email: 'Lucio_Hettinger@annie.ca'
+    email: 'Lucio_Hettinger@annie.ca',
+    rank: '2'
+
   },
   {
     id: 6,
     name: 'Mrs. Dennis Schulist',
     username: 'Leopoldo_Corkery',
-    email: 'Karley_Dach@jasper.info'
+    email: 'Karley_Dach@jasper.info',
+    rank: '1'
+
   },
   {
     id: 7,
     name: 'Kurtis Weissnat',
     username: 'Elwyn.Skiles',
-    email: 'Telly.Hoeger@billy.biz'
+    email: 'Telly.Hoeger@billy.biz',
+    rank: '1'
+
   },
   {
     id: 8,
     name: 'Nicholas Runolfsdottir V',
     username: 'Maxime_Nienow',
-    email: 'Sherwood@rosamond.me'
+    email: 'Sherwood@rosamond.me',
+    rank:'3'
+
   },
   {
     id: 9,
     name: 'Glenna Reichert',
     username: 'Delphine',
-    email: 'Chaim_McDermott@dana.io'
+    email: 'Chaim_McDermott@dana.io',
+    rank: '2'
+
   },
   {
     id: 10,
     name: 'Clementina DuBuque',
     username: 'Moriah.Stanton',
-    email: 'Rey.Padberg@karina.biz'
+    email: 'Rey.Padberg@karina.biz',
+    rank: '4'
   }
 ];
